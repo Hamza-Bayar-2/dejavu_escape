7e8fa97d0f95314fe83a138f5787684056c5f243
Hamza-Bayar-2 <hamzabayar234@gmail.com>
2025-09-19 01:45:40 +0300

    Add new badroom models and update Object Interactıon logic

--- Assets/Scripts/ObjectInteraction.cs
+++ Assets/Scripts/ObjectInteraction.cs
@@ -2,15 +2,39 @@ using UnityEngine;
 
 class ObjectInteraction : MonoBehaviour
 {
-  private Material myMaterial;
+  private Material[] myMaterials;
+  private Renderer myRenderer;
+  private Color[] originalColors;
+  [SerializeField] private float brightenAmount = 0.3f; // Ne kadar beyazlaştırılacak (0-1 arası)
 
   void Awake()
   {
-    // Get material from the renderer component
-    myMaterial = GetComponent<Renderer>().material;
+    // First try to get renderer from this object
+    myRenderer = GetComponent<Renderer>();
 
-    // Disable emission
-    myMaterial.DisableKeyword("_EMISSION");
+    // If not found, look in children
+    if (myRenderer == null)
+    {
+      myRenderer = GetComponentInChildren<Renderer>();
+    }
+
+    // If still not found, log error
+    if (myRenderer == null)
+    {
+      Debug.LogError("No Renderer found on " + gameObject.name + " or its children!");
+      return;
+    }
+
+    myMaterials = myRenderer.materials;
+    originalColors = new Color[myMaterials.Length];
+
+    // Store original colors
+    for (int i = 0; i < myMaterials.Length; i++)
+    {
+      originalColors[i] = myMaterials[i].color;
+    }
+
+    Debug.Log("Found Renderer on: " + myRenderer.gameObject.name + " with " + myMaterials.Length + " materials");
   }
 
   void OnTriggerEnter(Collider other)
@@ -25,21 +49,28 @@ class ObjectInteraction : MonoBehaviour
 
   private void Glow(Collider other)
   {
-    if (other.gameObject.CompareTag("Crosshair"))
+    if (other.gameObject.CompareTag("Crosshair") && myMaterials != null)
     {
-      myMaterial.EnableKeyword("_EMISSION");
-      Debug.Log("Glow");
+      // Brighten colors for all materials
+      for (int i = 0; i < myMaterials.Length; i++)
+      {
+        Color brightenedColor = Color.Lerp(originalColors[i], Color.white, brightenAmount);
+        myMaterials[i].color = brightenedColor;
+      }
+      Debug.Log("Glow - Brightened colors for " + myMaterials.Length + " materials");
     }
   }
 
   private void StopGlow(Collider other)
   {
-    if (other.gameObject.CompareTag("Crosshair"))
+    if (other.gameObject.CompareTag("Crosshair") && myMaterials != null)
     {
-      myMaterial.DisableKeyword("_EMISSION");
-      Debug.Log("StopGlow");
+      // Restore original colors for all materials
+      for (int i = 0; i < myMaterials.Length; i++)
+      {
+        myMaterials[i].color = originalColors[i];
+      }
+      Debug.Log("StopGlow - Restored original colors for " + myMaterials.Length + " materials");
     }
   }
-
-
 }
